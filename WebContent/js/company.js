dfd2f8cc688cd096402592ad6158091b07ae00fa
wanderyt <wanderyt@hotmail.com>
2013-06-13 11:23:30 +0800

    Change on Eclipse

--- WebContent/js/company.js
+++ WebContent/js/company.js
@@ -4,8 +4,8 @@
 var nsesh = nsesh || {};
 
 $(function() {
-  //show tabs
-    $('.bulletinMenuTabs').tabs().addClass( "ui-tabs-vertical ui-helper-clearfix" );
-    
-    //console.log(getCommonParent($('#logo'), $('#search')));
+    // show tabs
+    $('.bulletinMenuTabs').tabs().addClass("ui-tabs-vertical ui-helper-clearfix");
+
+    // console.log(getCommonParent($('#logo'), $('#search')));
 });
